53112d01b2f8e11e7080297de195e921298ca5c7
Carl Dennis <c.dennis@rhythmicsound.co.uk>
2022-04-05 15:31:15 +0100

    added build and push image workflow for cloud build

--- localbuild.sh
+++ localbuild.sh
@@ -5,17 +5,18 @@
 #See: http://manpages.ubuntu.com/manpages/trusty/man1/bash.1.html (search: 'Run  command  with  args')
 if [[ -x "$(command -v podman)" ]]; then
   
-    podman build -f Dockerfile -t statussentry . \
-    --build-arg PROJECT_ID=statusSentry \
-    --build-arg CONFIG_LOCATION="https://configlocation.com" \
-    --build-arg TWITTER_TOKEN=XXX \
-    --build-arg OUTBOUND_URL="https://someurl.com" \
-    --build-arg GOOGLE_APPLICATION_CREDENTIALS=XXX \
-    --build-arg STATUS_UPDATE_TOPIC=statusUpdate \
-    --build-arg PING_RESPONSE_TOPIC=pinger \
-    --build-arg STATUS_CHECK_ONLY=false \
-    --build-arg PINGER_ONLY=false \
-    --build-arg PORT=8080 
+    podman build -f Dockerfile -t statussentry \
+    --build-arg PROJECT_ID="${PROJECT_ID}" \
+    --build-arg CONFIG_LOCATION="${CONFIG_LOCATION}" \
+    --build-arg TWITTER_TOKEN="${TWITTER_TOKEN}" \
+    --build-arg OUTBOUND_URL="${OUTBOUND_URL}" \
+    --build-arg GOOGLE_APPLICATION_CREDENTIALS="${GOOGLE_APPLICATION_CREDENTIALS}" \
+    --build-arg STATUS_UPDATE_TOPIC="${STATUS_UPDATE_TOPIC}" \
+    --build-arg PING_RESPONSE_TOPIC="${PING_RESPONSE_TOPIC}" \
+    --build-arg STATUS_CHECK_ONLY="${STATUS_CHECK_ONLY}" \
+    --build-arg PINGER_ONLY="${PINGER_ONLY}" \
+    --build-arg PORT="${PORT}" \
+    .
     
     podman run --rm --name sentry -t -p 8080:8080 -p 8099:8099 statussentry
 
@@ -24,17 +25,19 @@ if [[ -x "$(command -v podman)" ]]; then
 
 elif [[ -x "$(command -v docker)" ]]; then
   
-    docker build -f Dockerfile -t statussentry . \
-    --build-arg PROJECT_ID=statusSentry \
-    --build-arg CONFIG_LOCATION="https://configlocation.com" \
-    --build-arg TWITTER_TOKEN=XXX \
-    --build-arg OUTBOUND_URL="https://someurl.com" \
-    --build-arg GOOGLE_APPLICATION_CREDENTIALS=XXX \
-    --build-arg STATUS_UPDATE_TOPIC=statusUpdate \
-    --build-arg PING_RESPONSE_TOPIC=pinger \
-    --build-arg STATUS_CHECK_ONLY=false \
-    --build-arg PINGER_ONLY=false \
-    --build-arg PORT=8080 
+    docker build -f Dockerfile -t statussentry \
+    --build-arg PROJECT_ID="${PROJECT_ID}" \
+    --build-arg CONFIG_LOCATION="${CONFIG_LOCATION}" \
+    --build-arg TWITTER_TOKEN="${TWITTER_TOKEN}" \
+    --build-arg OUTBOUND_URL="${OUTBOUND_URL}" \
+    --build-arg GOOGLE_APPLICATION_CREDENTIALS="${GOOGLE_APPLICATION_CREDENTIALS}" \
+    --build-arg STATUS_UPDATE_TOPIC="${STATUS_UPDATE_TOPIC}" \
+    --build-arg PING_RESPONSE_TOPIC="${PING_RESPONSE_TOPIC}" \
+    --build-arg STATUS_CHECK_ONLY="${STATUS_CHECK_ONLY}" \
+    --build-arg PINGER_ONLY="${PINGER_ONLY}" \
+    --build-arg PORT="${PORT}" \
+    .
+
     docker run --rm --name sentry -dt -p 8080:8080 -p 8099:8099 statussentry
 
     #cleanup
